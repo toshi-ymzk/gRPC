24ab1141d4a6d1fa0a61be6460130d8293522acd
Toshihiro Yamazaki <yamazaki.toshihiro@icloud.com>
2020-07-08 18:10:58 +0900

    Create an Xcode project

---
+++ gRPC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gRPC
+//
+//  Created by Toshihiro Yamazaki on 2020/07/08.
+//  Copyright © 2020 Toshihiro Yamazaki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
